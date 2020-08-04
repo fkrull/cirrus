efe258f79623ec126d9b15615f07baf9a5ad1908
Felix Krull <f_krull@gmx.de>
2020-08-04 20:06:57 +0200

    Fix dummy keyring impl

--- src/secrets/keyring_disabled.rs
+++ src/secrets/keyring_disabled.rs
@@ -1,9 +1,10 @@
 use crate::secrets::SecretValue;
+use anyhow::anyhow;
 
-pub(super) fn get_secret(name: &str) -> anyhow::Result<SecretValue> {
+pub(super) fn get_secret(_name: &str) -> anyhow::Result<SecretValue> {
     Err(anyhow!("OS keyring support is not enabled"))
 }
 
-pub(super) fn set_secret(name: &str, value: SecretValue) -> anyhow::Result<()> {
+pub(super) fn set_secret(_name: &str, _value: SecretValue) -> anyhow::Result<()> {
     Err(anyhow!("OS keyring support is not enabled"))
 }

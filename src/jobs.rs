fd4dbe0d41e5640ccff2e0d8e5f574740045b010
Felix Krull <f_krull@gmx.de>
2020-08-10 00:17:40 +0200

    WIP skeleton for a job queue

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,170 +1,114 @@
 use crate::model::backup;
-use chrono::{DateTime, Utc};
-use std::sync::Mutex;
+use crate::Timestamp;
+use futures::future::select_all;
+use futures::prelude::*;
+use futures::select;
+use log::{info, warn};
+use std::fmt::Debug;
+use std::future::Future;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum JobDescription {
+    Backup { definition: backup::Definition },
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum JobStatus {
-    Waiting,
     Running,
-    FinishedWithError,
+    Successful,
+    Error,
+}
+
+impl JobStatus {
+    fn is_running(&self) -> bool {
+        match self {
+            JobStatus::Running => true,
+            _ => false,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Job {
-    pub name: backup::Name,
-    pub definition: backup::Definition,
+    pub id: u64,
+    pub description: JobDescription,
     pub status: JobStatus,
-    pub last_start: Option<DateTime<Utc>>,
-    pub last_finish: Option<DateTime<Utc>>,
+    pub started: Timestamp,
+    pub finished: Option<Timestamp>,
 }
 
 impl Job {
-    pub fn new(name: backup::Name, definition: backup::Definition) -> Self {
-        Job {
-            name,
-            definition,
-            status: JobStatus::Waiting,
-            last_start: None,
-            last_finish: None,
-        }
-    }
-
-    pub fn running(&self) -> bool {
-        self.status == JobStatus::Running
-    }
-
-    pub fn set_started(&mut self, start: DateTime<Utc>) {
-        self.status = JobStatus::Running;
-        self.last_start = Some(start);
-    }
-
-    pub fn set_finished_successful(&mut self, end: DateTime<Utc>) {
-        self.status = JobStatus::Waiting;
-        self.last_finish = Some(end);
-    }
-
-    pub fn set_finished_failed(&mut self, end: DateTime<Utc>) {
-        self.status = JobStatus::FinishedWithError;
-        self.last_finish = Some(end);
+    fn is_finished(&self) -> bool {
+        !self.status.is_running()
     }
 }
 
-#[derive(Debug, Default)]
-pub struct JobsRepo {
-    jobs: Mutex<Vec<Job>>,
+trait RunningJob: Debug {
+    fn next(&mut self) -> Box<dyn Future<Output = Job> + Unpin>;
 }
 
-impl JobsRepo {
-    pub fn new(backups: impl IntoIterator<Item = (backup::Name, backup::Definition)>) -> Self {
-        let jobs = backups
-            .into_iter()
-            .map(|(name, definition)| Job::new(name, definition))
-            .collect();
-        JobsRepo {
-            jobs: Mutex::new(jobs),
-        }
+#[derive(Debug)]
+pub struct Jobs {
+    recv: UnboundedReceiver<JobDescription>,
+    running_jobs: Vec<Box<dyn RunningJob>>,
+    jobs: Vec<Job>,
+}
+
+impl Jobs {
+    pub fn new() -> (Jobs, JobsQueue) {
+        let (send, recv) = unbounded_channel();
+        let jobs = Jobs {
+            recv,
+            running_jobs: Vec::new(),
+            jobs: Vec::new(),
+        };
+        let jobs_queue = JobsQueue(send);
+        (jobs, jobs_queue)
     }
 
-    pub fn jobs(&self) -> impl Iterator<Item = Job> {
-        self.jobs.lock().unwrap().clone().into_iter()
-    }
+    // TODO: get jobs out
 
-    pub fn update(&self, jobs: impl Iterator<Item = Job>) {
-        let mut vec = self.jobs.lock().unwrap();
-        for job in jobs {
-            if let Some(old) = vec.iter_mut().find(|old| old.name == job.name) {
-                *old = job;
+    pub async fn run_jobs(&mut self) {
+        //while let Some(desc) = self.recv.recv().await {}
+        loop {
+            select! {
+                (job, idx, _) = select_all(self.running_jobs.iter_mut().map(|x| x.next())).fuse() => {
+                    if job.is_finished() {
+                        self.running_jobs.remove(idx);
+                    }
+                    // TODO: externalize somewhere else
+                    let idx = job.id as usize;
+                    self.jobs[idx] = job;
+                }
+                maybe_desc = self.recv.recv().fuse() => match maybe_desc {
+                    Some(desc) => {
+                        match desc {
+                            JobDescription::Backup { definition } => {
+                                todo!()
+                            }
+                        }
+                    },
+                    None => {
+                        info!("stopping job runner because all send ends were closed");
+                        break;
+                    }
+                }
             }
         }
     }
-
-    pub fn get(&self, name: &backup::Name) -> Option<Job> {
-        self.jobs
-            .lock()
-            .unwrap()
-            .iter()
-            .find(|&job| &job.name == name)
-            .cloned()
-    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::model::repo;
-    use maplit::hashmap;
-    use std::iter;
+#[derive(Debug, Clone)]
+pub struct JobsQueue(UnboundedSender<JobDescription>);
 
-    #[test]
-    fn should_get_and_update_jobs() -> anyhow::Result<()> {
-        let name1 = backup::Name("1".to_string());
-        let name2 = backup::Name("2".to_string());
-        let definition1 = backup::Definition {
-            repository: repo::Name("repo1".to_string()),
-            ..Default::default()
-        };
-        let definition2 = backup::Definition {
-            repository: repo::Name("repo2".to_string()),
-            ..Default::default()
-        };
-        let timestamp = Utc::now();
-
-        let repo = JobsRepo::new(hashmap! {
-            name1.clone() => definition1.clone(),
-            name2.clone() => definition2.clone(),
-        });
-
-        let mut jobs1 = repo.jobs().collect::<Vec<_>>();
-        jobs1.sort_by_key(|job| job.name.clone());
-        assert_eq!(
-            jobs1,
-            vec![
-                Job::new(name1.clone(), definition1.clone()),
-                Job::new(name2.clone(), definition2.clone()),
-            ]
-        );
-
-        let mut job = jobs1.remove(1);
-        job.set_started(timestamp);
-        repo.update(iter::once(job));
-
-        let mut jobs2 = repo.jobs().collect::<Vec<_>>();
-        jobs2.sort_by_key(|job| job.name.clone());
-        assert_eq!(
-            jobs2,
-            vec![
-                Job::new(name1.clone(), definition1.clone()),
-                Job {
-                    status: JobStatus::Running,
-                    last_start: Some(timestamp),
-                    ..Job::new(name2.clone(), definition2.clone())
-                },
-            ]
-        );
-
-        Ok(())
-    }
-
-    #[test]
-    fn should_get_job_by_name() -> anyhow::Result<()> {
-        let backup1 = (backup::Name("1".to_string()), backup::Definition::default());
-        let backup2 = (backup::Name("2".to_string()), backup::Definition::default());
-        let repo = JobsRepo::new(vec![backup1.clone(), backup2.clone()]);
-
-        let job = repo.get(&backup::Name("1".to_string())).unwrap();
-
-        assert_eq!(job.name, backup1.0);
-        assert_eq!(job.definition, backup1.1);
-        Ok(())
-    }
-
-    #[test]
-    fn should_not_get_missing_job_by_name() -> anyhow::Result<()> {
-        let repo = JobsRepo::new(iter::empty());
-
-        let result = repo.get(&backup::Name("test".to_string()));
-
-        assert_eq!(result, None);
-        Ok(())
+impl JobsQueue {
+    pub fn enqueue(&self, desc: JobDescription) {
+        if let Err(err) = self.0.send(desc) {
+            warn!(
+                "enqueuing a job failed (was the job runner shut down?): {}",
+                err
+            );
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@ use crate::model::Config;
 use crate::restic::Restic;
 use crate::secrets::Secrets;
 
-//pub mod jobs;
 pub mod commands;
+pub mod jobs;
 pub mod model;
 //pub mod pause;
 pub mod restic;
 //pub mod scheduler;
 pub mod secrets;
+
+pub type Timestamp = chrono::DateTime<chrono::Utc>;
 
 #[derive(Debug)]
 pub struct Cirrus {

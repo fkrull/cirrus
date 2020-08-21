7e5c61cfdfc6a42000c0dd61657c4add61b67ce2
Felix Krull <f_krull@gmx.de>
2020-08-22 01:36:30 +0200

    Impl templates

---
+++ cirrus-web/src/assets/templates.rs
@@ -0,0 +1,169 @@
+use log::error;
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::{ContentType, Status},
+    response::{Content, Responder},
+    Request, Rocket,
+};
+use serde::Serialize;
+use std::{borrow::Cow, path::Path};
+use tera::{Context, Tera};
+
+#[derive(Serialize)]
+pub(crate) struct NoContext {}
+
+#[derive(Debug)]
+pub(crate) struct Template {
+    name: Cow<'static, str>,
+    context: Context,
+}
+
+impl Template {
+    pub(crate) fn render(name: impl Into<Cow<'static, str>>, context: impl Serialize) -> Template {
+        let name = name.into();
+        let context = Context::from_serialize(context)
+            .map_err(|err| {
+                error!("failed to serialize context: {}", err);
+                err
+            })
+            .unwrap_or_default();
+        Template { name, context }
+    }
+
+    pub(crate) fn fairing() -> impl Fairing {
+        TemplateFairing
+    }
+}
+
+impl<'r> Responder<'r, 'static> for Template {
+    fn respond_to(self, req: &'r Request<'_>) -> rocket::response::Result<'static> {
+        let tera = req
+            .managed_state::<templates_impl::TeraState>()
+            .ok_or_else(|| {
+                error!("template state is missing from app");
+                Status::InternalServerError
+            })?;
+        let content_type = Path::new(&self.name.as_ref())
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .and_then(|ext| ContentType::from_extension(ext))
+            .unwrap_or_default();
+        let render = tera.render(&self.name, &self.context).map_err(|err| {
+            error!("failed to render template: {}", err);
+            Status::InternalServerError
+        })?;
+        Content(content_type, render).respond_to(req)
+    }
+}
+
+#[derive(Debug)]
+struct TemplateFairing;
+
+#[cfg(not(feature = "bundled-assets"))]
+mod templates_impl {
+    use super::*;
+    use rocket::Data;
+    use std::{path::Path, sync::RwLock};
+
+    #[derive(Debug)]
+    pub(super) struct TeraState(RwLock<Tera>);
+
+    impl TeraState {
+        fn reload(&self) {
+            if let Err(err) = self.0.write().unwrap().full_reload() {
+                error!("template reload failed: {}", err);
+            }
+        }
+
+        pub(super) fn render(&self, name: &str, context: &Context) -> anyhow::Result<String> {
+            self.0
+                .read()
+                .unwrap()
+                .render(name, context)
+                .map_err(|e| e.into())
+        }
+    }
+
+    #[rocket::async_trait]
+    impl Fairing for TemplateFairing {
+        fn info(&self) -> Info {
+            Info {
+                name: "Templates",
+                kind: Kind::Attach | Kind::Request,
+            }
+        }
+
+        async fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
+            let tera = Path::new(env!("CARGO_MANIFEST_DIR"))
+                .join("templates")
+                .join("**")
+                .to_str()
+                .and_then(|path| match Tera::new(path) {
+                    Ok(tera) => Some(tera),
+                    Err(err) => {
+                        error!("failed to create template context: {}", err);
+                        None
+                    }
+                });
+            match tera {
+                Some(tera) => Ok(rocket.manage(TeraState(RwLock::new(tera)))),
+                None => Err(rocket),
+            }
+        }
+
+        async fn on_request(&self, req: &mut Request<'_>, _data: &Data) {
+            if let Some(tera) = req.managed_state::<TeraState>() {
+                tera.reload();
+            }
+        }
+    }
+}
+
+#[cfg(feature = "bundled-assets")]
+mod templates_impl {
+    use super::*;
+    use include_dir::{include_dir, Dir, DirEntry};
+
+    #[derive(Debug)]
+    pub(super) struct TeraState(Tera);
+
+    impl TeraState {
+        pub(super) fn render(&self, name: &str, context: &Context) -> anyhow::Result<String> {
+            self.0.render(name, context).map_err(|e| e.into())
+        }
+    }
+
+    const TEMPLATE_FILES: Dir = include_dir!("templates");
+
+    #[rocket::async_trait]
+    impl Fairing for TemplateFairing {
+        fn info(&self) -> Info {
+            Info {
+                name: "Templates",
+                kind: Kind::Attach,
+            }
+        }
+
+        async fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
+            let bundled_templates =
+                TEMPLATE_FILES
+                    .find("**")
+                    .unwrap()
+                    .filter_map(|entry| match entry {
+                        DirEntry::File(file) => {
+                            Some((file.path().to_str().unwrap(), file.contents_utf8().unwrap()))
+                        }
+                        DirEntry::Dir(_) => None,
+                    });
+
+            let mut tera = Tera::default();
+            match tera.add_raw_templates(bundled_templates) {
+                Ok(()) => Ok(rocket.manage(TeraState(tera))),
+                Err(err) => {
+                    error!("failed to create template context: {}", err);
+                    Err(rocket)
+                }
+            }
+        }
+    }
+}
--- cirrus-web/src/lib.rs
+++ cirrus-web/src/lib.rs
@@ -1,16 +1,19 @@
+use crate::assets::templates::{NoContext, Template};
 use rocket::{get, routes};
 
+mod assets;
 mod static_files;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello Index"
+fn index() -> Template {
+    Template::render("index.html", NoContext {})
 }
 
 pub async fn launch() -> anyhow::Result<()> {
     rocket::ignite()
         .mount("/", routes![index])
         .mount("/static", static_files::StaticFiles)
+        .attach(Template::fairing())
         .launch()
         .await?;
     Ok(())

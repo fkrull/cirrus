042c5244e068d6e87e4f23dec7e6dd49cc55cd83
Felix Krull <f_krull@gmx.de>
2020-08-21 23:06:47 +0200

    Implement static files serving

--- cirrus-web/src/lib.rs
+++ cirrus-web/src/lib.rs
@@ -1,11 +1,17 @@
 use rocket::{get, routes};
 
+mod static_files;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello Index"
 }
 
 pub async fn launch() -> anyhow::Result<()> {
-    rocket::ignite().mount("/", routes![index]).launch().await?;
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/static", static_files::StaticFiles)
+        .launch()
+        .await?;
     Ok(())
 }

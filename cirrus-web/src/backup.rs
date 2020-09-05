c2d0e6d90b8eaad6a89d4040488df2209caba24d
Felix Krull <f_krull@gmx.de>
2020-09-05 22:49:12 +0200

    web: show more things

--- cirrus-web/src/backup.rs
+++ cirrus-web/src/backup.rs
@@ -3,26 +3,43 @@ use crate::{
     base::{base, BaseViewModel},
     Error,
 };
-use cirrus_core::model::{backup, repo};
+use cirrus_core::model::backup;
 use cirrus_daemon::Daemon;
-use rocket::{get, State};
+use rocket::{get, uri, State};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct BackupViewModel {
     base: BaseViewModel,
     name: String,
+    path: String,
+    excludes: Vec<String>,
+    repo: RepoForBackup,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RepoForBackup {
+    pub name: String,
+    pub link: String,
 }
 
 #[get("/backup/<name>")]
 pub async fn backup(name: String, daemon: State<'_, Daemon>) -> TemplateResult {
     let name = backup::Name(name);
     let definition = daemon.config.backups.get(&name).ok_or(Error::NotFound)?;
+    let repo_name = definition.repository.0.clone();
+    let repo_link = uri!(crate::repo::repo: name = &repo_name).to_string();
     Template::render(
         "backup.html",
         BackupViewModel {
             base: base(&daemon)?,
             name: name.0,
+            path: definition.path.0.clone(),
+            excludes: definition.excludes.iter().map(|x| x.0.clone()).collect(),
+            repo: RepoForBackup {
+                name: repo_name,
+                link: repo_link,
+            },
         },
     )
 }
--- cirrus-web/src/repo.rs
+++ cirrus-web/src/repo.rs
@@ -5,13 +5,21 @@ use crate::{
 };
 use cirrus_core::model::{backup, repo};
 use cirrus_daemon::Daemon;
-use rocket::{get, State};
+use rocket::{get, uri, State};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct RepoViewModel {
     base: BaseViewModel,
     name: String,
+    url: String,
+    backups: Vec<BackupForRepo>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct BackupForRepo {
+    pub name: String,
+    pub link: String,
 }
 
 #[get("/repo/<name>")]
@@ -22,11 +30,25 @@ pub async fn repo(name: String, daemon: State<'_, Daemon>) -> TemplateResult {
         .repositories
         .get(&name)
         .ok_or(Error::NotFound)?;
+    let backups = daemon
+        .config
+        .backups
+        .0
+        .iter()
+        .filter(|(_, backup)| backup.repository == name)
+        .map(|(name, _)| name.0.clone())
+        .map(|name| {
+            let link = uri!(crate::backup::backup: name = &name).to_string();
+            BackupForRepo { name, link }
+        })
+        .collect();
     Template::render(
         "repo.html",
         RepoViewModel {
             base: base(&daemon)?,
             name: name.0,
+            url: definition.url.0.clone(),
+            backups,
         },
     )
 }

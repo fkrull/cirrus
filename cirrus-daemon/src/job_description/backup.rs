a3ea93e7bb8d4315b1c60ecd66c8caec6c22b24d
Felix Krull <f_krull@gmx.de>
2020-10-19 21:21:54 +0200

    Implement backup jobs

--- cirrus-daemon/src/job_description/backup.rs
+++ cirrus-daemon/src/job_description/backup.rs
@@ -1,8 +1,15 @@
 use super::QueueId;
-use cirrus_core::model::{backup, repo};
+use cirrus_core::{
+    model::{backup, repo},
+    restic::Restic,
+    secrets::Secrets,
+};
+use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct BackupDescription {
+    pub restic: Arc<Restic>,
+    pub secrets: Arc<Secrets>,
     pub repo_name: repo::Name,
     pub backup_name: backup::Name,
     pub repo: repo::Definition,
@@ -18,6 +25,21 @@ impl BackupDescription {
     }
 
     pub(super) async fn start_job(self) {
-        todo!()
+        use cirrus_core::restic::Options;
+
+        // TODO don't unwrap errors
+        let repo_with_secrets = self.secrets.get_secrets(&self.repo).unwrap();
+        let process = self
+            .restic
+            .backup(
+                repo_with_secrets,
+                &self.backup,
+                &Options {
+                    capture_output: false,
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+        process.wait().await.unwrap();
     }
 }

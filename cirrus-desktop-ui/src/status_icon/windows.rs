9915d1f95991f06969f3d08ccf019bce134fe802
Felix Krull <f_krull@gmx.de>
2020-11-04 21:15:04 +0100

    Update icon & add light windows tray icon

--- cirrus-desktop-ui/src/status_icon/windows.rs
+++ cirrus-desktop-ui/src/status_icon/windows.rs
@@ -5,7 +5,8 @@ use winit::{
     event_loop::{ControlFlow, EventLoop, EventLoopProxy},
 };
 
-const ICON: &[u8] = include_bytes!("../resources/icon.ico");
+const ICON_DARK: &[u8] = include_bytes!("../resources/cirrus-idle-dark.ico");
+const ICON_LIGHT: &[u8] = include_bytes!("../resources/cirrus-idle-light.ico");
 
 #[derive(Debug)]
 pub(crate) struct StatusIcon {
@@ -75,7 +76,7 @@ impl View {
         let tray_icon = trayicon::TrayIconBuilder::new()
             .sender_winit(evloop.create_proxy())
             .tooltip(&model.tooltip())
-            .icon_from_buffer(ICON)
+            .icon(get_icon_for_theme()?)
             .menu(trayicon::MenuBuilder::new().item("Exit", model::Event::Exit))
             .build()
             .map_err(|e| eyre::eyre!("failed to create tray icon: {:?}", e))?;
@@ -85,6 +86,38 @@ impl View {
     fn update(&mut self, model: &model::Model) -> eyre::Result<()> {
         self.tray_icon
             .set_tooltip(&model.tooltip())
-            .map_err(|e| eyre::eyre!("failed to set tooltip: {:?}", e))
+            .map_err(|e| eyre::eyre!("failed to set tooltip: {:?}", e))?;
+        self.tray_icon
+            .set_icon(&get_icon_for_theme()?)
+            .map_err(|e| eyre::eyre!("failed to set icon: {:?}", e))?;
+        Ok(())
     }
+}
+
+#[derive(Debug)]
+enum SystrayTheme {
+    Light,
+    Dark,
+}
+
+fn systray_theme() -> eyre::Result<SystrayTheme> {
+    use winreg::enums::HKEY_CURRENT_USER;
+    use winreg::RegKey;
+    let personalize = RegKey::predef(HKEY_CURRENT_USER)
+        .open_subkey("Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize")?;
+    let is_light: u32 = personalize.get_value("SystemUsesLightTheme")?;
+    if is_light != 0 {
+        Ok(SystrayTheme::Light)
+    } else {
+        Ok(SystrayTheme::Dark)
+    }
+}
+
+fn get_icon_for_theme() -> eyre::Result<trayicon::Icon> {
+    let buffer = match systray_theme()? {
+        SystrayTheme::Light => ICON_DARK,
+        SystrayTheme::Dark => ICON_LIGHT,
+    };
+    trayicon::Icon::from_buffer(buffer, None, None)
+        .map_err(|e| eyre::eyre!("failed to load icon: {:?}", e))
 }

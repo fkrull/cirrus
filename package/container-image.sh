be98ad88a4501fb250c187ad1f8ae0d6ecab6131
Felix Krull <f_krull@gmx.de>
2020-11-10 19:10:47 +0100

    ci: manifest list

---
+++ package/container-image.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+IMAGE=$1
+BASE_IMAGE=$2
+QEMU_USER=${3-}
+COMMIT_ARGS=${4-}
+
+function buildah_run {
+  if [ -n "$QEMU_USER" ]; then
+    buildah run -v $PWD:/qemu $1 -- /qemu/$QEMU_USER --execve /bin/sh -c "$2"
+  else
+    buildah run $1 -- /bin/sh -c "$2"
+  fi
+}
+
+ctr=$(buildah from "$BASE_IMAGE")
+
+chmod 0755 restic cirrus
+buildah copy $ctr restic cirrus /usr/bin/
+
+buildah_run $ctr "apk add --no-cache ca-certificates openssh-client"
+buildah_run $ctr "mkdir -p /cache /config/cirrus"
+buildah config --env XDG_CONFIG_HOME=/config $ctr
+buildah config --env XDG_CACHE_HOME=/cache $ctr
+buildah config --entrypoint /usr/bin/cirrus $ctr
+buildah config --volume /cache $ctr
+
+buildah commit $COMMIT_ARGS $ctr docker://registry.gitlab.com/fkrull/cirrus/cirrus:latest-linux-arm32v7

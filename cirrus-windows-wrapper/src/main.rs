90ffb8a095983c9c434ad597fb353df66ee8d9a3
Felix Krull <f_krull@gmx.de>
2020-11-14 17:00:26 +0100

    Exclude cirrus-windows-wrapper code on not-Windows

--- cirrus-windows-wrapper/src/main.rs
+++ cirrus-windows-wrapper/src/main.rs
@@ -1,14 +1,18 @@
 #![windows_subsystem = "windows"]
 
-use std::os::windows::process::CommandExt;
-use winapi::um::winbase;
-
-const CIRRUS_BINARY: &str = "cirrus.exe";
-
+#[cfg(windows)]
 fn main() {
+    use std::os::windows::process::CommandExt;
+    use winapi::um::winbase;
+
+    const CIRRUS_BINARY: &str = "cirrus.exe";
+
     std::process::Command::new(CIRRUS_BINARY)
         .args(std::env::args_os().skip(1))
         .creation_flags(winbase::CREATE_NO_WINDOW)
         .spawn()
         .unwrap();
 }
+
+#[cfg(not(windows))]
+fn main() {}

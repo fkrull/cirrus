b202dea838f20eb58767537a7290d2668d377aaf
Felix Krull <f_krull@gmx.de>
2020-11-14 14:55:24 +0100

    Add windows_subsystem=windows wrapper to launch daemon without console visible

---
+++ cirrus-windows-wrapper/src/main.rs
@@ -0,0 +1,14 @@
+#![windows_subsystem = "windows"]
+
+use std::os::windows::process::CommandExt;
+use winapi::um::winbase;
+
+const CIRRUS_BINARY: &str = "cirrus.exe";
+
+fn main() {
+    std::process::Command::new(CIRRUS_BINARY)
+        .args(std::env::args_os().skip(1))
+        .creation_flags(winbase::DETACHED_PROCESS)
+        .spawn()
+        .unwrap();
+}

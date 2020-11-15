352d744125fcec33122674670d12ab05458eb631
Felix Krull <f_krull@gmx.de>
2020-11-15 03:44:49 +0100

    package: add script to generate signing cert

---
+++ package/New-SelfSignedCert.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = $Stop;
+
+$thumbprint = (New-SelfSignedCertificate `
+    -Type Custom `
+    -Subject "CN=Cirrus, O=CirrusBackup" `
+    -KeyUsage DigitalSignature `
+    -FriendlyName "Cirrus" `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+).Thumbprint
+Write-Output "Enter pfx file password:"
+$password = Read-Host -AsSecureString
+Export-PfxCertificate -cert "Cert:\CurrentUser\My\$thumbprint" -FilePath certificate.pfx -Password $password
+Export-Certificate -cert "Cert:\CurrentUser\My\$thumbprint" -FilePath certificate-public.crt

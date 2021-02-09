d33dbaf5ca9857e4f8f2c7373063e68ce340d9c7
Felix Krull <f_krull@gmx.de>
2021-02-09 23:56:49 +0100

    cli: update help messages

--- src/cli.rs
+++ src/cli.rs
@@ -4,19 +4,19 @@ use std::path::PathBuf;
 /// A configuration-driven backup program based on restic.
 #[derive(Clap)]
 pub struct Cli {
-    /// set a custom configuration file path
+    /// Sets a custom configuration file path
     #[clap(short, long, env = "CIRRUS_CONFIG_FILE")]
     pub config_file: Option<PathBuf>,
 
-    /// set the configuration from a string
+    /// Sets the configuration from a string
     #[clap(long, env = "CIRRUS_CONFIG")]
     pub config_string: Option<String>,
 
-    /// set a custom app configuration file path
+    /// Sets a custom app configuration file path
     #[clap(long, env = "CIRRUS_APPCONFIG")]
     pub appconfig_file: Option<PathBuf>,
 
-    /// set the restic binary to use
+    /// Sets the restic binary to use
     #[clap(long)]
     pub restic_binary: Option<String>,
 
@@ -26,20 +26,20 @@ pub struct Cli {
 
 #[derive(Clap)]
 pub enum Cmd {
-    /// run a configured backup
+    /// Runs a configured backup
     Backup(backup::Cli),
 
-    /// print the active configuration
+    /// Prints the active configuration
     Config,
 
-    /// get and set secrets
+    /// Gets and sets secrets
     #[clap(alias = "secrets")]
     Secret(secret::Cli),
 
-    /// run custom restic commands on configured repositories
+    /// Runs custom restic commands on configured repositories
     Restic(restic::Cli),
 
-    /// commands specific to the desktop build
+    /// Commands specific to the desktop build
     #[cfg(feature = "desktop-commands")]
     Desktop(desktop::Cli),
 }
@@ -49,7 +49,7 @@ pub mod backup {
 
     #[derive(Clap)]
     pub struct Cli {
-        /// the backup to run
+        /// The backup to run
         #[clap(name = "BACKUP")]
         pub backup: String,
     }
@@ -66,25 +66,26 @@ pub mod secret {
 
     #[derive(Clap)]
     pub enum Cmd {
+        /// Sets a secret from the terminal
         Set(Set),
+
+        /// Lists all configured secrets and whether they are currently set
         List(List),
     }
 
-    /// set a secret from the terminal
     #[derive(Clap)]
     pub struct Set {
-        /// repository of the secret
+        /// Repository of the secret
         #[clap(name = "REPOSITORY")]
         pub repository: String,
-        /// name of the secret
+        /// Name of the secret, or the repository password if not set
         #[clap(name = "SECRET")]
         pub secret: Option<String>,
     }
 
-    /// list all configured secrets and whether they are currently set
     #[derive(Clap)]
     pub struct List {
-        /// show passwords in clear text
+        /// Shows passwords in clear text
         #[clap(long)]
         pub show_passwords: bool,
     }
@@ -97,11 +98,11 @@ pub mod restic {
     #[derive(Clap)]
     #[clap(setting(AppSettings::TrailingVarArg))]
     pub struct Cli {
-        /// the cirrus repository to use with restic
+        /// The cirrus repository to use with restic
         #[clap(short, long, env = "CIRRUS_REPOSITORY")]
         pub repository: Option<String>,
 
-        /// command-line arguments to pass to restic
+        /// Command-line arguments to pass to restic
         #[clap(setting(ArgSettings::AllowHyphenValues))]
         pub cmd: Vec<OsString>,
     }
@@ -119,10 +120,10 @@ pub mod desktop {
 
     #[derive(Clap)]
     pub enum Cmd {
-        /// open the config file in the default editor
+        /// Opens the config file in the default editor
         OpenConfigFile,
 
-        /// open the app config file in the default editor
+        /// Opens the app config file in the default editor
         OpenAppconfigFile,
     }
 }

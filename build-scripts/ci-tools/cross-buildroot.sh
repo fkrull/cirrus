0be2dfd34c47a4474bf31a4ce15fb0a4b5272947
Felix Krull <f_krull@gmx.de>
2021-02-28 15:45:01 +0100

    ci: fix linkers maybe?

--- build-scripts/ci-tools/cross-buildroot.sh
+++ build-scripts/ci-tools/cross-buildroot.sh
@@ -10,7 +10,8 @@ apt-get update
 apt-get install -y \
     libc6-dev:$DEB_ARCH \
     libgcc-8-dev:$DEB_ARCH \
-    libdbus-1-dev:$DEB_ARCH
+    libdbus-1-dev:$DEB_ARCH \
+    gcc-$GCC_ARCH
 
 ln -s libgcc_s.so.1 /lib/$GCC_ARCH/libgcc_s.so
 

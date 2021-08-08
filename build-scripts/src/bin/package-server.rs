3859a4aeffebb838cc39dd3f81633f3e68bd8e23
Felix Krull <f_krull@gmx.de>
2021-08-08 16:22:26 +0200

    build-scripts: fix image build

--- build-scripts/src/bin/package-server.rs
+++ build-scripts/src/bin/package-server.rs
@@ -37,7 +37,7 @@ fn main() -> eyre::Result<()> {
     buildah_run(
         &ctr,
         qemu.as_ref(),
-        "apt-get update && apt-get install --no-install-recommends -y ca-certificates libdbus-1-3 openssh-client && apt-cache clean && rm -rf /var/lib/apt",
+        "apt-get update && apt-get install --no-install-recommends -y ca-certificates libdbus-1-3 openssh-client && apt-get clean && rm -rf /var/lib/apt",
     )?;
     cmd!("buildah config --env XDG_CONFIG_HOME=/config {ctr}").run()?;
     cmd!("buildah config --env XDG_DATA_HOME=/data/data {ctr}").run()?;

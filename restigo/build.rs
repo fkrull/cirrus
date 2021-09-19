aaab76701268af236616c61ab49d2abd482d42c8
Felix Krull <f_krull@gmx.de>
2021-09-19 02:19:01 +0200

    restigo: explicit error on msvc targets

--- restigo/build.rs
+++ restigo/build.rs
@@ -5,6 +5,11 @@ fn main() {
     println!("cargo:rerun-if-changed=go.sum");
     println!("cargo:rerun-if-changed=vendor/");
 
+    let is_msvc = std::env::var("TARGET").unwrap().ends_with("-msvc");
+    if is_msvc {
+        panic!("This crate does not work on -msvc targets (cgo only supports gcc on Windows");
+    }
+
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
     gobuild::Build::new()
         .file("main.go")

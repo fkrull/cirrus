12b47a81e5d45722dc63fc2ee85893133bd17629
Felix Krull <f_krull@gmx.de>
2022-01-26 00:49:48 +0100

    main: don't create Tokio runtime when running internal restic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-#[tokio::main]
-async fn main() -> eyre::Result<()> {
+fn main() -> eyre::Result<()> {
     #[cfg(feature = "restigo")]
     if std::env::var_os("__CIRRUS_INTERNAL_MODE_BUNDLED_RESTIC").is_some() {
         restigo::restic_main();
     }
 
-    cirrus::main().await
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?
+        .block_on(cirrus::main())?;
+    Ok(())
 }

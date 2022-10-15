094e0d6421e1bd7c49774b74556f2298b1774c25
Felix Krull <f_krull@gmx.de>
2022-10-15 16:16:43 +0200

    ci: try building the container image

--- build-scripts/src/bin/package-server.rs
+++ build-scripts/src/bin/package-server.rs
@@ -12,18 +12,11 @@ struct Args {
     /// rust compile target
     #[argh(option)]
     target: String,
-    /// QEMU binary (must support --execve flag), may be empty
-    #[argh(option)]
-    qemu_binary: Option<String>,
 }
 
 fn main() -> eyre::Result<()> {
     let sh = Shell::new()?;
     let args: Args = argh::from_env();
-    let qemu = args
-        .qemu_binary
-        .filter(|s| !s.is_empty())
-        .unwrap_or_else(no_qemu);
     let image_arch = TargetVars::for_target(&args.target)?.image_arch;
     let binaries_tar = Path::new(&args.binaries_tar);
     let context_path = binaries_tar
@@ -37,16 +30,10 @@ fn main() -> eyre::Result<()> {
         "buildah build-using-dockerfile
             --build-arg=IMAGE_ARCH={image_arch}
             --build-arg=TARBALL={tarball}
-            --volume={qemu}:/qemu:z,ro
             --tag=cirrus-server-image
             --file=Containerfile
             {context_path}"
     )
     .run()?;
     Ok(())
-}
-
-fn no_qemu() -> String {
-    const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
-    format!("{}/no-qemu.sh", CARGO_MANIFEST_DIR)
 }

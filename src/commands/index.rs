60b06cc0aa68a6dfa7741c3614d0aac81355fdd5
Felix Krull <f_krull@gmx.de>
2022-10-31 01:13:35 +0100

    main: refactor cache

--- src/commands/index.rs
+++ src/commands/index.rs
@@ -1,32 +1,15 @@
-use crate::cli;
+use crate::{cli, Cache};
 use cirrus_core::{
     config::{repo, Config},
     restic::Restic,
     secrets::Secrets,
 };
-use dirs_next as dirs;
-use std::path::PathBuf;
-
-const CACHEDIR_TAG_FILENAME: &str = "CACHEDIR.TAG";
-
-const CACHEDIR_TAG_CONTENT: &str = "Signature: 8a477f597d28d172789f06886806bc55
-# This file is a cache directory tag created by cirrus.
-# For information about cache directory tags see https://bford.info/cachedir/
-";
-
-async fn cache_dir() -> eyre::Result<PathBuf> {
-    let path = dirs::cache_dir()
-        .ok_or_else(|| eyre::eyre!("can't determine cache directory"))?
-        .join("cirrus");
-    tokio::fs::create_dir_all(&path).await?;
-    tokio::fs::write(path.join(CACHEDIR_TAG_FILENAME), CACHEDIR_TAG_CONTENT).await?;
-    Ok(path)
-}
 
 pub async fn fill(
     restic: &Restic,
     secrets: &Secrets,
     config: &Config,
+    cache: &Cache,
     args: cli::index::Fill,
 ) -> eyre::Result<()> {
     let repo_name = repo::Name(args.repo);
@@ -35,11 +18,10 @@ pub async fn fill(
         .get(&repo_name)
         .ok_or_else(|| eyre::eyre!("unknown repository {}", repo_name.0))?;
     let repo_with_secrets = secrets.get_secrets(repo)?;
-    let cache_dir = cache_dir().await?;
-    let mut db = cirrus_index::Database::new(&cache_dir, &repo_name).await?;
+    let cache_dir = cache.get().await?;
+    let mut db = cirrus_index::Database::new(cache_dir, &repo_name).await?;
     let snapshots = cirrus_index::index_snapshots(restic, &mut db, &repo_with_secrets).await?;
-    println!("{snapshots} snapshots indexed");
-
+    println!("{snapshots} snapshots saved");
     let unindexed = db.get_unindexed_snapshots(100).await?;
     println!("indexing {} unindexed snapshots...", unindexed.len());
     for snapshot in &unindexed {

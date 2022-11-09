48a183ac16ea17aae877d86b25ddc66fd7c3dc66
Felix Krull <f_krull@gmx.de>
2022-11-09 23:26:03 +0100

    daemon: fix shutdown when no receivers

--- cirrus-daemon/src/shutdown.rs
+++ cirrus-daemon/src/shutdown.rs
@@ -13,7 +13,10 @@ pub struct ShutdownAcknowledged;
 
 const SHUTDOWN_GRACE_PERIOD: Duration = Duration::from_secs(5);
 
-fn shutdown() -> ! {
+fn shutdown(graceful: bool) {
+    if !graceful {
+        tracing::warn!("grace period elapsed before all receivers acknowledged shutdown");
+    }
     tracing::info!("shutting down");
     std::process::exit(0);
 }
@@ -23,15 +26,26 @@ events::subscriptions! {
     ShutdownAcknowledged,
 }
 
-#[derive(Debug)]
 pub struct ShutdownService {
     events: Subscriptions,
+    grace_period: Duration,
+    shutdown: Box<dyn FnMut(bool) + Send>,
 }
 
 impl ShutdownService {
     pub fn new(events: &mut events::Builder) -> Self {
+        Self::new_internal(events, SHUTDOWN_GRACE_PERIOD, Box::new(shutdown))
+    }
+
+    fn new_internal(
+        events: &mut events::Builder,
+        grace_period: Duration,
+        shutdown: Box<dyn FnMut(bool) + Send>,
+    ) -> Self {
         ShutdownService {
             events: Subscriptions::subscribe(events),
+            grace_period,
+            shutdown,
         }
     }
 
@@ -39,11 +53,14 @@ impl ShutdownService {
     pub async fn run(&mut self) -> eyre::Result<()> {
         let _ = self.events.RequestShutdown.recv().await?;
         tracing::info!(
-            grace_period_secs = SHUTDOWN_GRACE_PERIOD.as_secs_f64(),
+            grace_period_secs = self.grace_period.as_secs_f64(),
             "shutdown requested"
         );
-        let grace_deadline = Instant::now() + SHUTDOWN_GRACE_PERIOD;
+        let grace_deadline = Instant::now() + self.grace_period;
         let mut required_acks = self.events.send(ShutdownRequested { grace_deadline });
+        if required_acks == 0 {
+            (self.shutdown)(true);
+        }
         loop {
             tokio::select! {
                 ack = self.events.ShutdownAcknowledged.recv() => {
@@ -51,14 +68,93 @@ impl ShutdownService {
                     required_acks -= 1;
                     tracing::debug!(required_acks, "received ack");
                     if required_acks == 0 {
-                        shutdown();
+                        (self.shutdown)(true);
                     }
                 }
                 _ = tokio::time::sleep_until(tokio::time::Instant::from_std(grace_deadline)) => {
-                    tracing::warn!("grace period elapsed before all receivers acknowledged shutdown");
-                    shutdown();
+                    (self.shutdown)(false);
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn should_exit_gracefully() {
+        let mut events = events::Builder::new_with_capacity(10);
+        let (send, mut recv) = tokio::sync::mpsc::unbounded_channel();
+        let mut shutdown_service = ShutdownService::new_internal(
+            &mut events,
+            Duration::from_nanos(1),
+            Box::new(move |graceful| {
+                send.send(graceful).unwrap();
+                panic!("done");
+            }),
+        );
+
+        let join = tokio::spawn(async move { shutdown_service.run().await });
+
+        events.typed_sender().send(RequestShutdown);
+        let graceful = recv.recv().await.unwrap();
+        assert!(graceful);
+        join.await.unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn should_exit_gracefully_once_all_acked() {
+        let mut events = events::Builder::new_with_capacity(10);
+        let (send, mut recv) = tokio::sync::mpsc::unbounded_channel();
+        let mut shutdown_service = ShutdownService::new_internal(
+            &mut events,
+            Duration::from_secs(5),
+            Box::new(move |graceful| {
+                send.send(graceful).unwrap();
+                panic!("done");
+            }),
+        );
+        let mut sub1 = events.subscribe::<ShutdownRequested>();
+        let mut sub2 = events.subscribe::<ShutdownRequested>();
+
+        let join = tokio::spawn(async move { shutdown_service.run().await });
+
+        events.typed_sender().send(RequestShutdown);
+        sub1.recv().await.unwrap();
+        events.typed_sender().send(ShutdownAcknowledged);
+        sub2.recv().await.unwrap();
+        events.typed_sender().send(ShutdownAcknowledged);
+        let graceful = recv.recv().await.unwrap();
+        assert!(graceful);
+        join.await.unwrap_err();
+    }
+
+    #[tokio::test]
+    async fn should_exit_ungracefully_if_missing_ack() {
+        let mut events = events::Builder::new_with_capacity(10);
+        let (send, mut recv) = tokio::sync::mpsc::unbounded_channel();
+        let mut shutdown_service = ShutdownService::new_internal(
+            &mut events,
+            Duration::from_millis(50),
+            Box::new(move |graceful| {
+                send.send(graceful).unwrap();
+                panic!("done");
+            }),
+        );
+        let mut sub1 = events.subscribe::<ShutdownRequested>();
+        let mut sub2 = events.subscribe::<ShutdownRequested>();
+
+        let join = tokio::spawn(async move { shutdown_service.run().await });
+
+        events.typed_sender().send(RequestShutdown);
+        sub1.recv().await.unwrap();
+        events.typed_sender().send(ShutdownAcknowledged);
+        sub2.recv().await.unwrap();
+        // no second ack
+        let graceful = recv.recv().await.unwrap();
+        assert!(!graceful);
+        join.await.unwrap_err();
     }
 }

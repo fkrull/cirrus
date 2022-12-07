5ff504c2af392e852f13830fcddbae46348e8d80
Felix Krull <f_krull@gmx.de>
2022-12-07 01:00:19 +0100

    index: fix async, kinda

--- cirrus-index/src/db.rs
+++ cirrus-index/src/db.rs
@@ -5,10 +5,6 @@ use rusqlite::{params, CachedStatement, Connection, OptionalExtension, Transacti
 use std::path::Path;
 use time::OffsetDateTime;
 
-fn b<T>(f: impl FnOnce() -> T) -> T {
-    tokio::task::block_in_place(f)
-}
-
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Default, serde::Serialize, serde::Deserialize)]
 #[serde(transparent)]
 struct FileId(i64);
@@ -28,29 +24,41 @@ pub struct Database {
 
 impl Database {
     pub async fn new(cache_dir: &Path, repo: &repo::Name) -> eyre::Result<Self> {
+        tokio::task::block_in_place(|| Database::new_sync(cache_dir, repo))
+    }
+
+    fn new_sync(cache_dir: &Path, repo: &repo::Name) -> eyre::Result<Self> {
         let file_path = cache_dir.join(format!("index-{}.sqlite", repo.0));
-        let mut conn = b(|| Connection::open(&file_path))?;
-        b(|| {
-            conn.pragma_update(None, "journal_mode", "wal")?;
-            conn.pragma_update(None, "synchronous", "normal")?;
-            conn.pragma_update(None, "foreign_keys", "on")?;
-            Ok::<_, eyre::Report>(())
-        })?;
-        b(|| migrations().to_latest(&mut conn))?;
+        let mut conn = Connection::open(&file_path)?;
+        conn.pragma_update(None, "journal_mode", "wal")?;
+        conn.pragma_update(None, "synchronous", "normal")?;
+        conn.pragma_update(None, "foreign_keys", "on")?;
+        migrations().to_latest(&mut conn)?;
         Ok(Database { conn })
     }
 
     pub async fn get_snapshots(&mut self) -> eyre::Result<Vec<Snapshot>> {
+        tokio::task::block_in_place(|| self.get_snapshots_sync())
+    }
+
+    fn get_snapshots_sync(&mut self) -> eyre::Result<Vec<Snapshot>> {
         //language=SQLite
         let mut stmt = self
             .conn
             .prepare_cached("SELECT * FROM snapshots ORDER BY time DESC")?;
-        let rows = b(|| stmt.query(()))?;
+        let rows = stmt.query(())?;
         let snapshots = serde_rusqlite::from_rows(rows).collect::<Result<_, _>>()?;
         Ok(snapshots)
     }
 
     pub async fn get_unindexed_snapshots(
+        &mut self,
+        newer_than: OffsetDateTime,
+    ) -> eyre::Result<Vec<Snapshot>> {
+        tokio::task::block_in_place(|| self.get_unindexed_snapshots_sync(newer_than))
+    }
+
+    fn get_unindexed_snapshots_sync(
         &mut self,
         newer_than: OffsetDateTime,
     ) -> eyre::Result<Vec<Snapshot>> {
@@ -65,7 +73,7 @@ WHERE (file_count IS NULL OR file_count = 0)
 GROUP BY snapshots.tree_hash
 ORDER BY MAX(snapshots.time) DESC",
         )?;
-        let rows = b(|| stmt.query([newer_than.unix_timestamp()]))?;
+        let rows = stmt.query([newer_than.unix_timestamp()])?;
         let snapshots = serde_rusqlite::from_rows(rows).collect::<Result<_, _>>()?;
         Ok(snapshots)
     }
@@ -86,12 +94,20 @@ GROUP BY snapshots.tree_hash
 ORDER BY MAX(snapshots.time) DESC
 LIMIT ?",
         )?;
-        let rows = b(|| stmt.query([limit]))?;
+        let rows = stmt.query([limit])?;
         let snapshots = serde_rusqlite::from_rows(rows).collect::<Result<_, _>>()?;
         Ok(snapshots)
     }
 
     pub async fn get_files(
+        &mut self,
+        parent: &Parent,
+        limit: u64,
+    ) -> eyre::Result<Vec<(File, Version, FileSnapshotMeta)>> {
+        tokio::task::block_in_place(|| self.get_files_sync(parent, limit))
+    }
+
+    fn get_files_sync(
         &mut self,
         parent: &Parent,
         limit: u64,
@@ -132,7 +148,7 @@ ORDER BY files.type, files.name
 LIMIT :limit",
         )?;
         let params = serde_rusqlite::to_params_named(Params { parent, limit })?;
-        let rows = b(|| stmt.query(&*params.to_slice()))?;
+        let rows = stmt.query(&*params.to_slice())?;
         let files = serde_rusqlite::from_rows::<RowResult>(rows)
             .map(|row| {
                 let row = row?;
@@ -146,26 +162,31 @@ LIMIT :limit",
         &mut self,
         snapshots: impl IntoIterator<Item = Snapshot>,
     ) -> eyre::Result<u64> {
-        let tx = b(|| self.conn.transaction())?;
+        tokio::task::block_in_place(|| self.import_snapshots_sync(snapshots))
+    }
+
+    fn import_snapshots_sync(
+        &mut self,
+        snapshots: impl IntoIterator<Item = Snapshot>,
+    ) -> eyre::Result<u64> {
+        let tx = self.conn.transaction()?;
         //language=SQLite
-        let prev_gen =
-            b(|| tx.query_row("SELECT generation FROM snapshots LIMIT 1", (), |r| r.get(0)))
-                .optional()?
-                .unwrap_or(0);
+        let prev_gen = tx
+            .query_row("SELECT generation FROM snapshots LIMIT 1", (), |r| r.get(0))
+            .optional()?
+            .unwrap_or(0);
         let generation = prev_gen + 1;
         let mut count = 0;
         for snapshot in snapshots {
             insert_snapshot(&tx, &snapshot, generation)?;
             count += 1;
         }
-        b(|| {
-            tx.execute(
-                //language=SQLite
-                "DELETE FROM snapshots WHERE generation != ? ",
-                [generation],
-            )
-        })?;
-        b(|| tx.commit())?;
+        tx.execute(
+            //language=SQLite
+            "DELETE FROM snapshots WHERE generation != ? ",
+            [generation],
+        )?;
+        tx.commit()?;
         Ok(count)
     }
 
@@ -174,11 +195,20 @@ LIMIT :limit",
         snapshot: &Snapshot,
         files: impl Stream<Item = eyre::Result<(File, Version)>>,
     ) -> eyre::Result<u64> {
-        let tx = b(|| self.conn.transaction())?;
+        tokio::task::block_in_place(|| self.import_files_sync(snapshot, files))
+    }
+
+    fn import_files_sync(
+        &mut self,
+        snapshot: &Snapshot,
+        files: impl Stream<Item = eyre::Result<(File, Version)>>,
+    ) -> eyre::Result<u64> {
+        let tx = self.conn.transaction()?;
         let tree_id = insert_tree(&tx, snapshot)?;
         let mut count = 0;
         tokio::pin!(files);
-        while let Some(file_and_version) = files.next().await {
+        let rt = tokio::runtime::Handle::current();
+        while let Some(file_and_version) = rt.block_on(files.next()) {
             let (file, version) = file_and_version?;
             let file_id = upsert_file(&tx, &file)?;
             let version_id = upsert_version(&tx, file_id, &version)?;
@@ -186,13 +216,11 @@ LIMIT :limit",
             count += 1;
         }
         //language=SQLite
-        b(|| {
-            tx.execute(
-                "UPDATE trees SET file_count = ? WHERE id = ?",
-                params![count, tree_id.0],
-            )
-        })?;
-        b(|| tx.commit())?;
+        tx.execute(
+            "UPDATE trees SET file_count = ? WHERE id = ?",
+            params![count, tree_id.0],
+        )?;
+        tx.commit()?;
         Ok(count)
     }
 }
@@ -233,7 +261,7 @@ VALUES (:generation,
         generation,
         snapshot,
     })?;
-    b(|| stmt.execute(&*params.to_slice()))?;
+    stmt.execute(&*params.to_slice())?;
     Ok(())
 }
 
@@ -243,8 +271,8 @@ fn insert_tree(tx: &Transaction<'_>, snapshot: &Snapshot) -> eyre::Result<TreeId
     //language=SQLite
     let mut stmt =
         tx.prepare_cached("INSERT INTO trees (hash, file_count) VALUES (?, 0) RETURNING id")?;
-    b(|| delete_stmt.execute([&snapshot.tree_hash.0]))?;
-    let id = b(|| stmt.query_row([&snapshot.tree_hash.0], |r| r.get(0)))?;
+    delete_stmt.execute([&snapshot.tree_hash.0])?;
+    let id = stmt.query_row([&snapshot.tree_hash.0], |r| r.get(0))?;
     Ok(TreeId(id))
 }
 
@@ -305,7 +333,7 @@ fn insert_version_tree_map(
     //language=SQLite
     let mut stmt =
         tx.prepare_cached("INSERT INTO version_tree_map (version, tree) VALUES (?, ?);")?;
-    b(|| stmt.execute([version.0, tree.0]))?;
+    stmt.execute([version.0, tree.0])?;
     Ok(())
 }
 
@@ -314,10 +342,12 @@ fn upsert(
     mut insert_stmt: CachedStatement<'_>,
     params: serde_rusqlite::NamedParamSlice,
 ) -> eyre::Result<i64> {
-    let id = b(|| get_stmt.query_row(&*params.to_slice(), |r| r.get(0))).optional()?;
+    let id = get_stmt
+        .query_row(&*params.to_slice(), |r| r.get(0))
+        .optional()?;
     let id = match id {
         Some(id) => id,
-        None => b(|| insert_stmt.query_row(&*params.to_slice(), |r| r.get(0)))?,
+        None => insert_stmt.query_row(&*params.to_slice(), |r| r.get(0))?,
     };
     Ok(id)
 }
